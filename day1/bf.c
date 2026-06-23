7f70a1d4cf7690c28e3504af0f8c11735edd2d07
jonahmer22 <jonah@jonahmerriam.net>
2026-06-22 23:10:02 -0400

    updated project file structure to seperate days

--- day1/bf.c
+++ day1/bf.c
@@ -15,23 +15,36 @@ uint8_t *data = NULL;
 size_t data_size = BASE_DATA_SIZE;
 
 void interpreter(const char *body){
-    char *head = body;
+    // pre validate bracket matching before executing anything
+    int depth = 0;
+    for(const char *p = body; *p != '\0'; p++){
+        if(*p == '[') depth++;
+        else if(*p == ']'){
+            if(--depth < 0){
+                fprintf(stderr, "[FATAL 0x%04X]: Unmatched ']'.\r\n", 0x9854);
+                return;
+            }
+        }
+    }
+    if(depth != 0){
+        fprintf(stderr, "[FATAL 0x%04X]: Unmatched '['.\r\n", 0x4089);
+        return;
+    }
+
+    char *head = (char *)body;
 
     while(*head != '\0'){
         switch(*head){
-            // instructions
             case '>':{
                 data_ptr++;
                 if(data_ptr >= data_size){
                     data_size *= 2;
-
                     data = realloc(data, data_size);
                 }
                 break;
             }
             case '<':{
                 if(data_ptr == 0){
-                    // don't hard error, but just let the user know
                     fprintf(stderr, "[ERROR 0x%04X]: Undefined behavior decrement past 0 of data pointer.\r\n", 0x4032);
                     break;
                 }
@@ -55,14 +68,11 @@ void interpreter(const char *body){
                 break;
             }
             case '[':{
-                // depth match looping (that was my problem)
                 if(data[data_ptr] == 0){
                     int depth = 1;
                     while(depth > 0 && *++head != '\0'){
-                        if(*head == '[')
-                            depth--;
-                        else if(*head == ']')
-                            depth++;
+                        if(*head == '[') depth++;
+                        else if(*head == ']') depth--;
                     }
                     if(depth != 0){
                         fprintf(stderr, "[FATAL 0x%04X]: Unmatched '['.\r\n", 0x7043);
@@ -75,18 +85,14 @@ void interpreter(const char *body){
                 if(data[data_ptr] != 0){
                     int depth = 1;
                     while(depth > 0 && --head >= body){
-                        if(*head == '[')
-                            depth--;
-                        else if(*head == ']')
-                            depth++;
+                        if(*head == ']') depth++;
+                        else if(*head == '[') depth--;
                     }
                 }
-
                 if(head < body){
-                    fprintf(stderr, "[FATAL 0x%04X]: Unmatched ']'.\r\n". 0x9343);
+                    fprintf(stderr, "[FATAL 0x%04X]: Unmatched ']'.\r\n", 0x9343);
                     exit(EXIT_FAILURE);
                 }
-
                 break;
             }
             default:{
@@ -94,41 +100,66 @@ void interpreter(const char *body){
             }
         }
 
+#ifdef DEBUG
+        fprintf(stderr, "data_ptr: %llu\ndata: ", (unsigned long long)data_ptr);
+        for(size_t i = 0; i <= data_ptr; i++){
+            fprintf(stderr, "%d ", data[i]);
+        }
+        puts("");
+#endif
+
         head++;
     }
-    #ifdef DEBUG
-    fprintf(stderr, "data_ptr: %llu\ndata: ", (long long)data_ptr);
-    for(size_t i = 0; i <= data_ptr; i++){
-        fprintf(stderr, "%d ", data[i]);
+}
+
+static char *read_file(const char *path){
+    FILE *f = fopen(path, "r");
+    if(!f){
+        fprintf(stderr, "[FATAL]: Cannot open file '%s'.\r\n", path);
+        exit(EXIT_FAILURE);
     }
-    puts("");
-    #endif
+
+    fseek(f, 0, SEEK_END);
+    long len = ftell(f);
+    rewind(f);
+
+    char *buf = malloc(len + 1);
+    fread(buf, 1, len, f);
+    buf[len] = '\0';
+
+    fclose(f);
+
+    return buf;
 }
 
 int main(int argc, char **argv){
-    // there is a file to open
+    data = calloc(BASE_DATA_SIZE, 1);
+
     if(argc > 1){
-        // open the file and run the interpreter over the contents
+        char *src = read_file(argv[1]);
+
+        interpreter(src);
+
+        free(src);
+        free(data);
+
         return EXIT_SUCCESS;
     }
-
-    data = realloc(data, data_size);
 
     // repl
     while(true){
         fprintf(stdout, "?> ");
+        fflush(stdout);
 
-        // get the input from repl
         char line[1024] = {0};
-        fgets(line, 1024, stdin);
+        if(!fgets(line, 1024, stdin)) break;
 
-        if(memcmp(line, "bye", 3) == 0){
-            break;
-        }
+        if(memcmp(line, "bye", 3) == 0) break;
 
         interpreter(line);
     }
 
     free(data);
+
     return EXIT_SUCCESS;
 }
